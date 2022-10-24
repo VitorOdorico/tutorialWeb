2f9c740f6cb09078a45babcd4c285953224db8cf
VitorOdorico <ve743137@gmail.com>
2022-10-24 15:22:28 -0300

    add alerts

---
+++ udemy-cursos/a fazer/Curso de JavaScript e TypeScript/js para iniciantes/marcadoresAritimeticos.js
@@ -0,0 +1,16 @@
+const num1 = 5;
+const num2 = 10;
+
+
+let soma = num1 + num2;
+    console.log(soma)
+let subtracao = num1 - num2;
+    console.log(subtracao)
+let divisao = num1 / num2;
+    console.log(divisao)
+let multiplicaco = num1 * num2;
+    console.log(multiplicaco)
+let potenciacao = num1 ** 4;
+    console.log(potenciacao)
+let resto = num1 % 2;
+    console.log(resto)
