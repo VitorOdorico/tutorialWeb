6600a081e70813d9d9b847977cd68eb25681fd5d
VitorOdorico <ve743137@gmail.com>
2022-10-24 10:01:37 -0300

    start courser js

---
+++ udemy-cursos/a fazer/Curso de JavaScript e TypeScript/js para iniciantes/var_let.js
@@ -0,0 +1,6 @@
+let nameJoao = 'João';
+let esposaName = 'maria';
+
+var history = (
+    console.log(nameJoao, 'nasceu em 1984,', 'Em 2000', nameJoao,'conheceu ', esposaName,'.', nameJoao, ' casou-se com', esposaName, 'em 2012',esposaName, 'teve 1 filho com', nameJoao, 'em 2015, o filho de ' , nameJoao, 'se chama eduardo.' )
+);
