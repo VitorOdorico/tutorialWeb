e58c02c2796e46b91e71476c7022a4d6feccd2c7
VitorOdorico <ve743137@gmail.com>
2022-10-24 12:07:10 -0300

    finish const

---
+++ udemy-cursos/a fazer/Curso de JavaScript e TypeScript/js para iniciantes/const.js
@@ -0,0 +1,17 @@
+// não pode mudar o valor da constante.
+const nome = 'vitor';
+console.log(nome)
+
+// da para se usar constantes para atribuir valor atrelado nas vereaveis
+
+const numberOne = 5;
+const numberTwo = 10;
+let result = (numberOne * numberTwo);
+
+    console.log(result);
+
+const resultDouble = result * 2;
+    console.log(resultDouble)
+
+const resultTriplic = result * 3;
+    console.log(resultTriplic)
