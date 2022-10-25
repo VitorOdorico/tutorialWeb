cacf14d5da261c5fd8e68c3330ec42d536aa8c57
VitorOdorico <ve743137@gmail.com>
2022-10-25 10:26:20 -0300

    start part of the logic

---
+++ udemy-cursos/a fazer/Curso de JavaScript e TypeScript/logica/operadores de comparação.js
@@ -0,0 +1,24 @@
+/*
+Operadores de comparação
+    = atribuição de valor
+    > maior que
+    < menor que
+    >= maior igual a
+    <= menor igual a
+    == igual
+    === estritamente igual
+    != diferente
+    !== estritamente diferente
+*/
+
+let a = 5;
+let b = 10;
+
+console.log('maior que:', + a > b);
+console.log('menor que:,' + a < b);
+console.log('maior igual a:', + a >= b);
+console.log('menor igual a:', + a <= b);
+console.log('igual a:', + a == b);
+console.log('estritamente igual:', + a === b);
+console.log('diferente:', + a != b);
+console.log('estritamente diferente:', + a !== b );
