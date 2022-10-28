e507bcfd384817b4d026dac920adfd88d7a43bfa
VitorOdorico <ve743137@gmail.com>
2022-10-28 14:18:52 -0300

    algoritmos

---
+++ youtube_curso/algoritmos em c#/vareaveis.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class progam{
+     static void Main(){
+        char verdadeiro = 's';
+        // vareavel de caracteres um unico caractere
+
+
+        string nome = "vitor";
+        // é uma vereavel para armazenar conjuntos de caracteres
+
+
+        int num = 10;
+        // numeros inteiros com sinal podendo positivos e negativos
+
+
+        float pi = 3.14f;
+        // coloca-se o f para definir que o valor é de float
+
+
+        double pi = 3.14567;
+
+        boll verdadeiro = true;
+        boll falso = false;
+        
+
+
+// é uma vereavel inteira sem sinal com o limite entre 0 e 255
+
+     }
+}
