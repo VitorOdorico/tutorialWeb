0be3816757accf5ca642503ef0cc75f370e32bb1
VitorOdorico <ve743137@gmail.com>
2022-10-31 15:22:49 -0300

    test python

---
+++ udemy-cursos/logica/python/aula1.py
@@ -0,0 +1,25 @@
+include pysas from ast import If
+from pysas
+
+def inserction():
+    inject = ()
+    for inject in inserction:
+        print(inserction())
+
+text = ''
+line = ''
+
+def test(text, line):
+    text = 'testinInserction'
+    line = 43
+    if(text == line):
+        print(test())
+        
+        
+inLine = [test()]
+
+if inLine == 0:
+    print( "incorrect")
+else:
+    print("correct")
+    
