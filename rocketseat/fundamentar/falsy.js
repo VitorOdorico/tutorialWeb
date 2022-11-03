8a04c90c88d8e2c3e03f9cf3d37564b4189bf89f
VitorOdorico <ve743137@gmail.com>
2022-11-03 14:40:44 -0300

    add falsy.js

---
+++ rocketseat/fundamentar/falsy.js
@@ -0,0 +1,13 @@
+/*
+    Falsy
+        É quando um valor é considerado false em contextos onde um bollean é obrigatório(conditions and loops)
+
+        - false
+        - 0
+        - -0
+        - null
+        - undefined
+        - Nan
+*/
+
+console.log(NaN ? 'verdadeiro':'falso')
