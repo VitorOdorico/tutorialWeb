f2a3576ee74b39353b9ab675e1675af864c265d3
VitorOdorico <ve743137@gmail.com>
2022-12-29 13:59:39 -0300

    add to switch case to implementations

--- rocketseat/fundamentar/guia estrelar js/condicionais/switch.js
+++ rocketseat/fundamentar/guia estrelar js/condicionais/switch.js
@@ -1,37 +1,44 @@
-console.log('1 para prosseguir')
-console.log('2 para parar o processo');
-numberOne = 1;
-numberTwo = 2;
-let expression = numberOne;
+// console.log('1 para prosseguir')
+// console.log('2 para parar o processo');
+// numberOne = 1;
+// numberTwo = 2;
+// let expression = numberOne;
 
-switch(expression){
-    case 1:
-        console.log('Funcionou')
-        break
-    case 2:
-        console.log('processo encerrado')
-        break
-    default:
-        console.log("não é uma opção valida")
-        break
-}
+// switch(expression){
+//     case 1:
+//         console.log('Funcionou')
+//         break
+//     case 2:
+//         console.log('processo encerrado')
+//         break
+//     default:
+//         console.log("não é uma opção valida")
+//         break
+// }
 
 function calculator(numberOne, numberTwo, Operator){
     let result;
     let porcent;
     switch(Operator){
         case '+':
-            result = numberOne + numberTwo 
+            result = numberOne + numberTwo;
+            console.log(result)
             break
         case '-':
-            result = numberOne - numberTwo 
+            result = numberOne - numberTwo; 
+            console.log(result)
             break
         case '*':
-            result = numberOne * numberTwo 
+            result = numberOne * numberTwo; 
+            console.log(result)
             break
         case '/':
-            result = numberOne / numberTwo 
+            result = numberOne / numberTwo; 
+            console.log(result)
             break
-
+        default:
+            console.log('nao é um operador valido')
     }   
 }
+
+console.log(calculator(10,20,'+'))
